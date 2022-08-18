6c0adfd495a01717b16124d103deeecf9cb3064d
denizedizcan <70265563+denizedizcan@users.noreply.github.com>
2022-08-18 19:36:09 +0300

    user models

---
+++ api/models/user.go
@@ -0,0 +1,124 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+
+	"github.com/badoux/checkmail"
+)
+
+//user struct fields used in db
+type User struct {
+	UserID    uint64    `gorm:"primary_key;auto_increment" json:"user_id"`
+	Name      string    `gorm:"size:255;not null;unique" json:"name"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+}
+
+//user prepare values to insert or update
+func (u *User) Prepare() {
+	u.Name = html.EscapeString(strings.TrimSpace(u.Name))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.Password = html.EscapeString(strings.TrimSpace(u.Password))
+	u.CreatedAt = time.Now()
+}
+
+// validate values
+func (u *User) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "login":
+		if u.Password == "" {
+			return errors.New("required password")
+		}
+		if u.Email == "" {
+			return errors.New("required email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	case "create":
+		if u.Name == "" {
+			return errors.New("required Name")
+		}
+		if u.Password == "" {
+			return errors.New("required password")
+		}
+		if u.Email == "" {
+			return errors.New("required email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	default:
+		if u.Name == "" {
+			return errors.New("required Name")
+		}
+		if u.Password == "" {
+			return errors.New("required password")
+		}
+		if u.Email == "" {
+			return errors.New("required email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	}
+}
+
+//Crate user
+func (u *User) SaveUser(db *gorm.DB) error {
+
+	if result := db.Create(&u); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+//login user
+func (u *User) LoginUser(db *gorm.DB) error {
+
+	var user User
+
+	if result := db.Model(User{}).Where("email = ?", u.Email).First(&user); result.Error != nil {
+		return result.Error
+	}
+	if u.Password != user.Password {
+		return errors.New("faild to login wrong password")
+	}
+	return nil
+}
+
+//find user from db
+func (u *User) FindUser(db *gorm.DB) error {
+	if u.UserID != 0 {
+		if result := db.Model(&u).Preload(clause.Associations).Find(&u); result.Error != nil {
+			return result.Error
+		}
+	}
+	if result := db.Preload(clause.Associations).Model(&u).Where("email = ?", u.Email).Find(&u); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+// find user data and return it
+func (u *User) FindUserData(db *gorm.DB) (*User, error) {
+	if u.UserID != 0 {
+		if result := db.Model(User{}).Preload(clause.Associations).Find(&u); result.Error != nil {
+			return &User{}, result.Error
+		}
+	}
+	if result := db.Model(User{}).Preload(clause.Associations).Where("email = ?", u.Email).Find(&u); result.Error != nil {
+		return &User{}, result.Error
+	}
+	return u, nil
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/denizedizcan/REST-API-User-Management-Service
+
+go 1.17
+
+require (
+	github.com/badoux/checkmail v1.2.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	gorm.io/gorm v1.23.8
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
